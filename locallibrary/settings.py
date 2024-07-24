4c62e57cb8b2d99777eae3d8a2afb78577ed9eb5
Martijn Nieuwland <martijn@nieuwland.mn>
2024-07-24 21:43:03 +0200

    Allow pythonanywhere host

--- locallibrary/settings.py
+++ locallibrary/settings.py
@@ -36,7 +36,9 @@ SECRET_KEY = os.environ.get(
 # DEBUG = True
 DEBUG = os.environ.get("DJANGO_DEBUG", "") != "False"
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["martijnnieuwland.pythonanywhere.com", "127.0.0.1"]
+
+CSRF_TRUSTED_ORIGINS = ["https://martijnnieuwland.pythonanywhere.com"]
 
 
 # Application definition
